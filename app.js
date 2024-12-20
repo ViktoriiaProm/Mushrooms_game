674afa2049f266149d64421694c994f571742449
Viktoriia-P-H11 <viktoriia1ua@gmail.com>
2024-12-20 01:01:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const gameArea = document.getElementById('game');
+const scoreDisplay = document.getElementById('score');
+const livesDisplay = document.getElementById('lives');
+const startButton = document.getElementById('start-btn');
+let score=0;
+let lives=3;
+let gameInterval;
+let MushroomsData=[];
+async function fetchMushrooms(){
+    
+} 
